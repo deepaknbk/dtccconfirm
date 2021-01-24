d972dc414df2c1052e3bd3c0ab0787d5d43f747e
Deepak Nallajerla <deepaknbk@gmail.com>
2021-01-24 07:51:15 -0600

    cleaned code
    updated folder names
    removed unwanted fields

--- src/Solution.py
+++ src/Solution.py
@@ -1,36 +1,35 @@
 import os
 import collections
-import pandas as pd 
-import time
-import psutil
-import psutil
-#import mail
-import json
 import csv
 
 def parse_hdr(hdr):
-    region = hdr[4:5]
     sysid = hdr[5:10]
     submitter = hdr[15:20]
     sent_dt = hdr[26:34]
-    seq = hdr[59:63]
-    return region,sysid,submitter,sent_dt,seq
+    return sysid,submitter,sent_dt
 
 def parse_outbound_file(file):
     ORecord = collections.namedtuple(
         'ORecord',
-        'region,sysid,submitter,sent_dt,seq,participant,file_type,hdr'
+        'sysid,submitter,sent_dt,,participant,participant_name,file_type,hdr'
     )
     sysid_ref={
         '46029': 'FAR',
         '46030' : 'COM'
     }
-    region,sysid,submitter,sent_dt,seq,participant,file_type,hdr,temp_file_type=None,None,None,None,None,None,None,None,None
+
+    participant_ref={
+        '0122' : 'Abc',
+        '0987' : 'Def',
+        '0443' : 'Xyz'
+    }
+    sysid,submitter,sent_dt,participant,participant_name,file_type,hdr,temp_file_type=None,None,None,None,None,None,None,None
+
     with open(file, 'r') as reader:
         # Read and print the entire file line by line
         for line in reader:
             if line[:3]=='HDR':
-                region,sysid,submitter,sent_dt,seq=parse_hdr(line)
+                sysid,submitter,sent_dt,=parse_hdr(line)
                 hdr=line[:63]
 
             elif line[:3]=='C12' :
@@ -43,50 +42,52 @@ def parse_outbound_file(file):
 
     file_type=sysid_ref.get(sysid,temp_file_type)
 
+    participant_name=participant_ref.get(participant,participant)
+
     record=ORecord(
-        region, sysid, submitter, sent_dt, seq, participant, file_type ,hdr
+        sysid, submitter, sent_dt,  participant,participant_name, file_type ,hdr
     )
     return  record
 
 def parse_confirm_file(file):
     CRecord = collections.namedtuple(
         'CRecord',
-        'region,sysid,submitter,sent_dt,seq,status,hdr'
+        'sysid,submitter,sent_dt,,status,hdr'
     )
-    region,sysid,submitter,sent_dt,seq,status,hdr = None, None, None, None, None, None,None
+    sysid,submitter,sent_dt,status,hdr = None, None, None, None, None
 
     with open(file, 'r') as reader:
         # Read and print the entire file line by line
         for line in reader:
             if line[5:8]=='HDR':
-                region,sysid,submitter,sent_dt,seq=parse_hdr(line[5:])
+                sysid,submitter,sent_dt,=parse_hdr(line[5:])
                 hdr=line[5:68]
             elif line.find('ACCEPTED'):
                 status='Accepted'
             elif  line.find('REJECTED'):
                 status='REJECTED'
     record = CRecord(
-        region, sysid, submitter, sent_dt, seq, status ,hdr
+        sysid, submitter, sent_dt,  status ,hdr
     )
     return record
 
 def dtcc_confirm():
 
     #Reding input data files using command prompt
-    outbound_path= 'C:/Users/User/PycharmProjects/dtccconfirm/Input Files/'
+    outbound_path= 'C:/Users/User/PycharmProjects/dtccconfirm/outbounds/'
     outbound_files=os.listdir(outbound_path)
-    confirm_path='C:/Users/User/PycharmProjects/dtccconfirm/Result Files/'
+    confirm_path='C:/Users/User/PycharmProjects/dtccconfirm/confirms/'
+    output_path='C:/Users/User/PycharmProjects/dtccconfirm/output/'
     confirm_files = os.listdir(confirm_path)
-    print('Files:',outbound_files)
-    print(os.getcwd(),__file__)
+
     #Populating Participant ID,File Type Feilds in Output file from input data files
     outbound_data=[]
     confirm_data=[]
 
     dtcc_confirm_data = []
-    dtcc_confirm = collections.namedtuple(
+    Dtcc_Confirm = collections.namedtuple(
         'Dtcc_Confirm',
-        'region, file, sent_dt, participant, hdr'
+        'sysid,submitter,sent_dt,participant,participant_name,file_type,status'
     )
 
     for file in outbound_files:
@@ -98,17 +99,31 @@ def dtcc_confirm():
         #print(f'Processing Confirm file:{confirm_path+file}')
         confirm_record=parse_confirm_file(confirm_path+file)
         confirm_data.append(confirm_record)
-    with open('output.csv', mode='w',newline='') as output_file:
+    with open(output_path+'dtcc_outbound_status.csv', mode='w',newline='') as output_file:
         output_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        output_writer.writerow(['region','sysid','submitter','sent_dt','seq','participant','file_type','hdr','status'])
+        output_writer.writerow(['sysid','submitter','sent_dt','participant','participant_name','file_type','status'])
         for orecord in outbound_data:
              for crecord in confirm_data:
                  if orecord.hdr==crecord.hdr:
-                        frecord=orecord+(crecord.status,)
+                        #frecord=orecord+(crecord.status,)
+                        frecord=Dtcc_Confirm(
+                            orecord.sysid,
+                            orecord.submitter,
+                            orecord.sent_dt,
+                            orecord.participant,
+                            orecord.participant_name,
+                            orecord.file_type,
+                            crecord.status
+                        )
                         dtcc_confirm_data.append(frecord)
                         output_writer.writerow(frecord)
 
-    print(dtcc_confirm_data)
+def clean_up_files():
+    pass
 
+def unzip_files():
+    pass
 
+def archive_files():
+    pass
 
--- src/launcher.py
+++ src/launcher.py
@@ -1,7 +1,12 @@
 import Solution
 
 def main():
+
+    Solution.clean_up_files()
+    Solution.unzip_files()
     Solution.dtcc_confirm()
+    Solution.archive_files()
+
 
 if __name__=='__main__':
     main()
